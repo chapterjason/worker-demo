52626c4d6bfdc33581245910be07c6203bcfd7fc
chapterjason <jason@sourecode.dev>
2023-10-29 19:26:18 +0100

    Add worker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    SoureCode\Bundle\Daemon\SoureCodeDaemonBundle::class => ['all' => true],
+    SoureCode\Bundle\Worker\SoureCodeWorkerBundle::class => ['all' => true],
 ];
