93b03cc2fe06f002836265ac821d1ee2722e6a33
chapterjason <jason@sourecode.dev>
2023-10-30 22:10:07 +0100

    Add logging

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     SoureCode\Bundle\Worker\SoureCodeWorkerBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
